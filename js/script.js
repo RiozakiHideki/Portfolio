4430e3585d1a8661a2b79055050052bc0cf2b298
RiozakiHideki <romagonta@yandex.ru>
2023-06-12 18:28:29 +0300

    aga

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 var coin_button = document.getElementById("add_coins");
+var savedCoinsValue = localStorage.getItem("coinsValue");
+
+if (savedCoinsValue > 0) {
+  var coins = document.getElementById("total_coins");
+  coins.innerText = savedCoinsValue;
+}
 
 coin_button.addEventListener("click", function () {
   var coins = document.getElementById("total_coins");
   var coins_value = parseInt(coins.innerText, 10) + 1;
   coins.innerText = coins_value;
+
+  localStorage.setItem("coinsValue", coins_value);
 });
