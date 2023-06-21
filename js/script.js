1734f81bece5944edb2f21edfd6301f50d240de0
RiozakiHideki <romagonta@yandex.ru>
2023-06-21 15:33:58 +0300

    firebase_coins

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-var coin_button = document.getElementById("add_coins");
-var savedCoinsValue = localStorage.getItem("coinsValue");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  runTransaction,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-if (savedCoinsValue > 0) {
-  var coins = document.getElementById("total_coins");
-  coins.innerText = savedCoinsValue;
-}
+const appSettings = {
+  databaseURL: "https://portfolio-coins-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const coinsInDB = ref(database, "coins");
+const coin_button = document.getElementById("add_coins");
+const coin_value_from_DB = document.getElementById("total_coins");
+
+onValue(coinsInDB, (snapshot) => {
+  const coinsValue = snapshot.val();
+  coin_value_from_DB.textContent = coinsValue;
+});
 
 coin_button.addEventListener("click", function () {
-  var coins = document.getElementById("total_coins");
-  var coins_value = parseInt(coins.innerText, 10) + 1;
-  coins.innerText = coins_value;
-
-  localStorage.setItem("coinsValue", coins_value);
+  runTransaction(coinsInDB, (currentValue) => {
+    // Увеличиваем значение на 1
+    return (currentValue || 0) + 1;
+  });
 });
